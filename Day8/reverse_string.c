0a93dae40650111e5e76682b9b7e4adf6b21639a
Jeeveshgbs <jeeveshgbs@gmail.com>
2024-12-10 13:14:34 +0530

    update code

---
+++ Day8/reverse_string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,str[100];
+    printf("enter a number:  ");
+    scanf("%d",str);
+
+    while()
+    printf("The reverse of a number is : %d",revNumber);
+    return 0;
+}
